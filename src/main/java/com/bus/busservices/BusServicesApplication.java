7b86c32448f25d09439194d258d5a488755e4936
manikanta varma <manikanta.nadimpally@ikonicit.com>
2023-04-08 17:23:31 +0530

    Swagger OpenAPI Implementation

--- src/main/java/com/bus/busservices/BusServicesApplication.java
+++ src/main/java/com/bus/busservices/BusServicesApplication.java
@@ -1,10 +1,12 @@
 package com.bus.busservices;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@OpenAPIDefinition
 public class BusServicesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bus/busservices/controller/BusController.java
+++ src/main/java/com/bus/busservices/controller/BusController.java
@@ -2,6 +2,10 @@ package com.bus.busservices.controller;
 
 import com.bus.busservices.response.TrafikLabResponse;
 import com.bus.busservices.services.BusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +19,23 @@ public class BusController {
 
     @Autowired
     private BusService busService;
+
+    @Operation(summary = "This is to get the Bus model type")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Bus modelType found", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Bus not found", content = @Content)
+    })
     @GetMapping("/bus/{modelType}")
     public ResponseEntity<TrafikLabResponse> busService(@PathVariable String modelType){
         return new ResponseEntity<TrafikLabResponse>(busService.getBusService(modelType), HttpStatus.OK);
     }
 
     @GetMapping("/bus/services")
+    @Operation(summary = "This is to get Bus Service details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Bus Service details found", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Bus Service details not found", content = @Content)
+    })
     public ResponseEntity<String> busServiceDetails(){
         return new ResponseEntity<String>(busService.getBusService(), HttpStatus.OK);
     }
